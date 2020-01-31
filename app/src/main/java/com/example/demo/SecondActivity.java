19dc9e4e9b8d7eb4dcf478dc6779ee599fa22044
SAMONDOT <santhose37@gmail.com>
2020-01-31 09:40:16 +0530

    Navigation

--- app/src/main/java/com/example/demo/SecondActivity.java
+++ app/src/main/java/com/example/demo/SecondActivity.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,23 +7,21 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_second);
         move();
     }
     private void move(){
-        Button b1=findViewById(R.id.b1);
-        b1.setOnClickListener(new View.OnClickListener(){
+        Button b2=findViewById(R.id.b2);
+        b2.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
             }
         });
     }
